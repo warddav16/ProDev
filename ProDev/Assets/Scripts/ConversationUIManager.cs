ac82970a697b0b0e2f9003c4f5ea1bf009a513a2
warddav16 <warddav16@gmail.com>
2017-04-01 15:20:49 -0500

    working on conversations

---
+++ ProDev/Assets/Scripts/ConversationUIManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConversationUIManager : MonoBehaviour
+{
+    static ConversationUIManager _instance;
+    public static ConversationUIManager Instance() { return _instance; }
+    public GameObject player;
+
+    public GameObject UIRoot;
+    public Text SpeakerNameField;
+    public Text ConversationBox;
+    public Button[] answers;
+    public string PlayerName = "ShitDev";
+
+    private VIDE_Data _currentDialogue;
+
+    void Awake()
+    {
+        if (_instance)
+        {
+            Debug.LogError("Should not be 2 conversation ui managers in scene");
+        }
+        _instance = this;
+
+        _currentDialogue = gameObject.AddComponent<VIDE_Data>();
+        _currentDialogue.LoadDialogues();
+    }
+
+    void Start()
+    {
+        SetPlayerControllerActive(true);
+        UIRoot.SetActive(false);
+    }
+
+    void SetPlayerControllerActive(bool toSet)
+    {
+        player.GetComponent<CustomCharacterController>().enabled = toSet;
+        foreach( var comp in player.GetComponentsInChildren<RotateWithMouse>() )
+        {
+            Cursor.lockState = toSet ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !toSet;
+            comp.enabled = toSet;
+        }
+    }
+
+    public void StartConversation(VIDE_Assign diagToLoad)
+    {
+        _currentDialogue.OnActionNode += ActionHandler;
+        _currentDialogue.OnNodeChange += NodeChangeAction;
+        _currentDialogue.OnEnd += EndConversation;
+
+        SetPlayerControllerActive(false);
+        UIRoot.SetActive(true);
+        _currentDialogue.BeginDialogue( diagToLoad );
+    }
+
+    public void ActionHandler(int action)
+    {
+        Debug.Log("ACTION TRIGGERED: " + action.ToString());
+    }
+
+    void NodeChangeAction(VIDE_Data.NodeData data)
+    {
+        if (data.currentIsPlayer)
+        {
+            //SetOptions(data.playerComments);
+            SpeakerNameField.text = PlayerName;
+        }
+        else
+        {
+            SpeakerNameField.text = _currentDialogue.assigned.dialogueName;
+        }
+        ConversationBox.text = data.npcComment[data.npcCommentIndex];
+    }
+
+    public void EndConversation(VIDE_Data.NodeData data )
+    {
+        _currentDialogue.OnActionNode -= ActionHandler;
+        _currentDialogue.OnNodeChange -= NodeChangeAction;
+        _currentDialogue.OnEnd -= EndConversation;
+
+        SetPlayerControllerActive(true);
+        _currentDialogue.EndDialogue();
+        UIRoot.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ ProDev/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float MaxConversatioNDistance = 5.0f;
+    void Update()
+    {
+        if( Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit outInfo;
+            if( Physics.Raycast(ray, out outInfo, MaxConversatioNDistance))
+            {
+                VIDE_Assign assign = outInfo.collider.gameObject.GetComponent<VIDE_Assign>();
+                if( assign != null)
+                {
+                    ConversationUIManager.Instance().StartConversation(assign);
+                }
+            }
+        }
+    }
+}
